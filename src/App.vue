fbe923e088519980fb9d1572770c8b70c5b2a49d
Leona <1047504737@qq.com>
2025-04-12 11:41:43 +0800

    #feat: add logic for WordManagement

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="main">
-  <el-tabs v-model="activeName" @tab-click="handleClick">
+  <el-tabs v-model="activeName">
     <el-tab-pane label="航认字" name="first">
-      <WordManagement :words="words1"></WordManagement>
+      <WordManagement :words="words1"
+                      @update:words="(updatedWords) => handleWordsUpdate('words2', updatedWords)"
+      ></WordManagement>
     </el-tab-pane>
     <el-tab-pane label="皓皓认字" name="second">
-      <WordManagement :words="words2"></WordManagement>
+      <WordManagement :words="words2"
+                      @update:words="(updatedWords) => handleWordsUpdate('words2', updatedWords)"
+      ></WordManagement>
     </el-tab-pane>
     <el-tab-pane label="航古诗背诵" name="third">
       <PoetryManagement :poetryList="poetries1"></PoetryManagement>
@@ -22,10 +26,21 @@ import WordManagement from './components/WordManagement.vue'
 import PoetryManagement from './components/PoetryManagement.vue'
 import { ElTabs, ElTabPane } from 'element-plus'
 
-const words1 = ref(['一', '二', '三', '四', '五'])
-const words2 = ref(['六', '七', '八', '九', '十'])
+const activeName = ref('first')
+//todo 从文件读值
+const words1 = ref([{text:'一',status:true},{text:'二',status:false},{text:'三',status:''}])
+const words2 = ref([{text:'六',status:true}])
 const poetries1 = ref(['静夜思'])
 const poetries2 = ref(['春晓'])
+
+const handleWordsUpdate = (wordsRef, updatedWords) => {
+  // todo 修改值并写入文件
+  if (wordsRef === 'words1') {
+      words1.value = updatedWords
+    } else if (wordsRef === 'words2') {
+      words2.value = updatedWords
+    }
+}
 
 </script>
 
--- src/components/WordManagement.vue
+++ src/components/WordManagement.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="words-management">
-    <h1>{{ words.length }}</h1>
+    <el-row :wrap="true" :gutter="20">
+      <el-col :xs="24" :sm="12" :md="8" :lg="6">
+        <h2 >总数{{ words.length }} 未检查数{{words.length-recognizedCount-unrecognizedCount}}</h2>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="8" :lg="6">
+        <h2>认识数{{recognizedCount}} 不认识数{{unrecognizedCount}}</h2>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20" :wrap="true">
+      <el-col
+        v-for="(word, index) in words"
+        :key="index"
+        :xs="8"
+        :sm="6"
+        :md="4"
+        :lg="3"
+        :style="{ marginBottom: '20px' }"
+      >
+        <el-card shadow="hover"
+                 class="box-card"
+                 :body-class="word.status ? 'recognized' : (word.status === '' ? 'init' : 'unrecognized')">
+          <div class="word-item">
+            <div>{{ word.text }}</div>
+            <div style="display: flex;">
+              <el-checkbox-button
+                :model-value="word.status === true"
+                                  @change="() => updateStatus(word, index, true)">✔️</el-checkbox-button>
+              <el-checkbox-button
+                :model-value="word.status === false"
+                @change="() => updateStatus(word, index, false)">❌</el-checkbox-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -8,10 +42,61 @@
 export default {
   name: 'WordManagement',
   props: {
-    words: Array
+    words: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    recognizedCount() {
+      return this.words.filter(word => word.status === true).length;
+    },
+    unrecognizedCount() {
+      return this.words.filter(word => word.status === false).length;
+    },
+  },
+  methods: {
+    updateStatus(word, index, newStatus) {
+      const updatedWords = [...this.words];
+
+      if (newStatus === updatedWords[index].status) {
+        updatedWords[index].status = '';
+      } else {
+        updatedWords[index].status = newStatus;
+      }
+
+      this.$emit('update:words', updatedWords);
+    }
   }
 }
 </script>
 
 <style scoped>
+.words-management {
+  padding: 20px;
+}
+
+.words-container {
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.word-item {
+  font-size: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;  /* 垂直居中 */
+  justify-content: center;  /* 水平居中 */
+}
+:deep(.init) {
+  background-color: #f5f7fa;
+}
+:deep(.recognized) {
+  background-color: #67c23a;
+}
+:deep(.unrecognized) {
+  background-color: #ff6666;
+}
 </style>
